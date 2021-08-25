54bcb2751e5fae8682c4f67a69dd0f6887829b06
Emma Valor <emmavalor62@gmail.com>
2021-08-25 13:52:46 +0200

    first commit

--- espace_abonne.php
+++ espace_abonne.php
@@ -4,7 +4,8 @@
 			<meta charset="utf-8"/>
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
 			<link href='https://fonts.googleapis.com/css?family=Maitree' rel='stylesheet'>
-			<link rel="stylesheet" href="style2.css" />
+			<link rel="stylesheet" href="css/style2.css" />
+			<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 			<title>Espace Abonné</title>
 		</head>
 
@@ -17,13 +18,41 @@
 
 			<h2>Bienvenue dans votre espace abonné !</h2>
 
+
 			<div id="img">
 				<img src="image/utilisateur.png" alt="user image">
 			</div>
 
-			<div id="bienvenue">
+			<form>
+				<div class="row justify-content-center">
+					<div class="col-sm-4 text-center">
+						<div class="form-group">
+		  					<label class="col-form-label mt-4" for="inputDefault">Plaque Immatriculation</label>
+		 					<input type="text" class="form-control text-center" placeholder="AA-123-AA" id="inputDefault">
+						</div>
+						<div class="form-group">
+		  					<label class="col-form-label mt-4" for="inputDefault">Couleur</label>
+		 					<input type="text" class="form-control text-center" placeholder="Insérez la couleur" id="inputDefault">
+						</div>
+						<div class="form-group">
+		  					<label class="col-form-label mt-4" for="inputDefault">Marque</label>
+		 					<input type="text" class="form-control text-center" placeholder="Insérez la marque" id="inputDefault">
+						</div>
+						<div class="form-group">
+		  					<label class="col-form-label mt-4" for="inputDefault">Modèle</label>
+		 					<input type="text" class="form-control text-center" placeholder="Insérez le modèle" id="inputDefault">
+						</div>
+
+						<div class="d-grid gap-2">
+							<button class="btn btn-lg btn-outline-primary" id="btn" type="button">Ajouter</button>
+						</div>
+					</div>
+				</div>
+			</form>
+
+			<!-- <div id="bienvenue">
 				<p>Salut <span> <?php echo $_GET['pseudo']; ?> </span></p>
 				<p>N'oublies pas de te déconnecter :)</p>
 			</div>
-		</body>
+		</body> -->
 	</html>
